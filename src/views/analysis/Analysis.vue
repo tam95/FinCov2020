215bfd020cfaf628f396058cd50b2ab232c5f6b7
Tam Nguyen <tam.nguyen@sympa.com>
2020-03-22 14:46:42 +0200

    Analysis and report page init

---
+++ src/views/analysis/Analysis.vue
@@ -0,0 +1,41 @@
+<template>
+  <CRow>
+    <CCol>
+      <CCard>
+        <CCardHeader>
+          <h3>Analysis in Finland</h3>
+        </CCardHeader>
+      </CCard>
+      <CCard>
+        <CCardBody>
+          <MainChartExample style="height:300px;margin-top:40px;" />
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+import MainChartExample from "../charts/MainChartExample";
+
+export default {
+  name: "Users",
+  components: {
+    MainChartExample
+  },
+  data() {
+    return {};
+  },
+  watch: {
+    $route: {
+      immediate: true,
+      handler(route) {
+        if (route.query && route.query.page) {
+          this.activePage = Number(route.query.page);
+        }
+      }
+    }
+  },
+  methods: {}
+};
+</script>
---
+++ src/views/report/Report.vue
@@ -0,0 +1,33 @@
+<template>
+  <CRow>
+    <CCol col="12" xl="8">
+      <CCard>
+        <CCardHeader>
+          Report
+        </CCardHeader>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  data () {
+    return {
+    }
+  },
+  watch: {
+    $route: {
+      immediate: true,
+      handler (route) {
+        if (route.query && route.query.page) {
+          this.activePage = Number(route.query.page)
+        }
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
