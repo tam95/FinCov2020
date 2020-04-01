3fb4ff36d3dc36745db9b92f9098d4cd3d2c8604
Tam Nguyen <tam.nguyen@sympa.com>
2020-04-01 08:06:59 +0300

    report form (incomplete)

--- src/views/report/Report.vue
+++ src/views/report/Report.vue
@@ -1,10 +1,22 @@
 <template>
   <CRow>
-    <CCol col="12" xl="8">
+    <CCol>
       <CCard>
         <CCardHeader>
-          Report
+          <h3>Report</h3>
         </CCardHeader>
+      </CCard>
+      <CCard>
+        <CCardBody>
+          <CRow>
+            <h5 class="label-padding">Current symtomps</h5>
+            <CCol sm="12" md="4" lg="4">
+              <CInputCheckbox label="Cough" />
+              <CInputCheckbox label="Fever" />
+              <CInputCheckbox label="Sneezing" />
+            </CCol>
+          </CRow>
+        </CCardBody>
       </CCard>
     </CCol>
   </CRow>
@@ -12,22 +24,39 @@
 
 <script>
 export default {
-  name: 'Users',
-  data () {
-    return {
-    }
+  name: "Users",
+  data() {
+    return {};
   },
   watch: {
     $route: {
       immediate: true,
-      handler (route) {
+      handler(route) {
         if (route.query && route.query.page) {
-          this.activePage = Number(route.query.page)
+          this.activePage = Number(route.query.page);
         }
       }
     }
   },
-  methods: {
-  }
-}
+  methods: {}
+};
 </script>
+<style lang="css">
+.label-padding{
+  padding: 15px;
+}
+.checkbox-custom {
+    top: 0px;
+    left: 0px;
+    height: 24px;
+    width: 24px;
+    background-color: transparent;
+    border-radius: 5px;
+    transition: all 0.3s ease-out;
+    -webkit-transition: all 0.3s ease-out;
+    -moz-transition: all 0.3s ease-out;
+    -ms-transition: all 0.3s ease-out;
+    -o-transition: all 0.3s ease-out;
+    border: 2px solid #FFFFFF;
+}
+</style>
